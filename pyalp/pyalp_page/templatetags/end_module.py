66d038614b87abd0d7b54d9987c9b45ac028384b
Dominic May <jack.thatch@gmail.com>
2014-05-05 23:41:51 +0800

    Remove duplicate code

--- pyalp/pyalp_page/templatetags/end_module.py
+++ pyalp/pyalp_page/templatetags/end_module.py
@@ -1,7 +1,4 @@
-from os.path import exists
-
 from django import template
-from django.template.loader import render_to_string
 
 register = template.Library()
 
@@ -12,50 +9,7 @@ class EndModuleNode(ModuleNode):
     template_name = 'end_module.html'
 
     def __init__(self, module_type="main", bgcolor=""):
-        self.module_type = module_type
-        self.bgcolor = bgcolor
-
-    def render(self, context):
-        # global colors, container, master
-        skin = context['skin']
-
-        # globalize this for multiple formats??
-        if self.bgcolor:
-            current_color = self.bgcolor
-        else:
-            current_color = skin.colors["cell_background"]
-
-        sides = ["tl", "tr", "bl", "br", "t", "b", "lside", "rside"]
-        imgs = {
-            "mod" + side: exists(skin.asset_path + "mod" + side + ".gif")
-            for side in sides
-        }
-
-        dims = {}
-        for key, value in imgs.items():
-            if value:
-                width, height = skin.getimagesize(
-                    skin.asset_path + key + ".gif")
-            else:
-                width, height = (
-                    skin.container["border_width"],
-                    skin.container["border_height"]
-                )
-            dims[key] = {
-                'width': width,
-                'height': height
-            }
-
-        render_context = {
-            'dims': dims,
-            'current_color': current_color
-        }
-
-        return render_to_string(
-            'end_module.html',
-            render_context,
-            context_instance=context
-        )
+        super().__init__(module_type=module_type, bgcolor=bgcolor)
 
 
 @register.tag
