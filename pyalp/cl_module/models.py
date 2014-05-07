a5752ba88119faeaf94f90b710ea84c94feef058
Dominic May <jack.thatch@gmail.com>
2014-05-07 22:24:17 +0800

    Added Module model :P

---
+++ pyalp/cl_module/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Module(models.Model):
+    """
+    "modules" => "`moduleid` int(10) unsigned NOT NULL auto_increment,
+                `file` varchar(45) NOT NULL default '',
+                `ordernum` int(10) unsigned NOT NULL default '0',
+                `description` varchar(45) NOT NULL default '',
+                `required` varchar(45) default NULL,
+                PRIMARY KEY  (`moduleid`)",
+    """
+
+    # `file` varchar(45) NOT NULL default '',
+
+    # in this implementation, we store the name of a
+    # template tag instead
+    name = models.CharField(max_length=45)
+
+    # `ordernum` int(10) unsigned NOT NULL default '0',
+    ordernum = models.IntegerField()
+
+    enabled = models.BooleanField(default=True)
+
+    # `description` varchar(45) NOT NULL default '',
+    description = models.CharField(max_length=45)
+
+    # `required` varchar(45) default NULL,
+    required = models.CharField(max_length=45)
+
+    def __str__(self):
+        return '{} at position {}'.format(
+            self.name,
+            self.ordernum
+        )
