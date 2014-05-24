75c42071f0e499b5f2b0f304ebae7c879d69c5ab
Dominic May <jack.thatch@gmail.com>
2014-05-24 12:00:35 +0800

    Cache the skin css

--- pyalp/skins/views.py
+++ pyalp/skins/views.py
@@ -1,12 +1,16 @@
 from os.path import join
 
+from django.http import HttpResponse
 from django.template.context import Context
 from django.template.loader import select_template
-from django.http import HttpResponse
+from django.views.decorators.cache import cache_page
 
 from pyalp.skin import get_skin
 
+MINUTE = 60
 
+
+@cache_page(MINUTE * 15)
 def skin_css(request):
     skin = get_skin()
 
